a333838c23652d35ec04391b25d07d8f3e686997
Erik Ihrén <erikihr@gmail.com>
2015-07-25 20:08:16 +0200

    Forgot to add new volume module

---
+++ client/modules/Volume.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8-*-
+import random
+import re
+import os
+
+WORDS = ["VOLUME", "TURN", "UP", "THE", "SET", "MAX", "DOWN", "TO", "FULL", "MUTE"]
+
+
+def handle(text, mic, profile, data=None):
+    """
+        Responds to user-input, typically speech text.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+        mic -- used to interact with the user (for both input and output)
+        profile -- contains information related to the user (e.g., phone
+                   number)
+    """
+    print "In volume"
+    volume = 10
+    relative = ""
+    if "number" in data["outcomes"][0]["entities"]:
+        print "Changing absolute volume"
+        volume = data["outcomes"][0]["entities"]["number"][0]["value"]
+    elif "volume_relative" in data["outcomes"][0]["entities"]:
+        print "Changing relative volume"
+        value = data["outcomes"][0]["entities"]["volume_relative"][0]["value"]
+        if value == "up":
+            relative = "+"
+        elif value == "down":
+            relative = "-"
+        elif value == "max":
+            volume = 100
+        elif value == "mute":
+            volume = 0
+    cmd = "amixer set Master {0}%{1}".format(volume,relative)
+    os.system(cmd)
+
+
+def isValid(text):
+    """
+        Returns True if the input is related to the volume
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+    """
+    return "volume" in text.lower()
